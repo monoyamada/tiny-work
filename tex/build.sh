8601a2a77cee050ab3395ff834abb323947c904a
monoyamada <monoyamada@gmail.com>
2009-06-02 01:56:43 +0900

    added tex/

---
+++ tex/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+usage="$0 <tex-file-name-without-ext>"
+if [ $# -lt 1 ]; then
+	echo $usage
+	exit
+fi
+out_dir="output"
+opts="-draftmod -halt-on-error"
+opt_dir="-output-directory=$out_dir"
+latex $opts $opt_dir $1
