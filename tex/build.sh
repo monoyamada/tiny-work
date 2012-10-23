31a42348512083aa158c1d2e57352937f7bc9d2e
monoyamada <monoyamada@gmail.com>
2012-10-23 14:43:04 +0900

    edite tex

--- tex/build.sh
+++ tex/build.sh
@@ -14,5 +14,12 @@ fi
 
 opts_encode="-kanji=utf8"
 opts="-halt-on-error $opts_encode -output-directory=$out_dir"
-platex $opts $1
 pbibtex $opts_encode $out_dir/$1
+platex $opts $1
+
+if [ $# -gt 1 ]; then
+	if [ $2 = "pdf" ]; then
+		dvipdfmx -o $out_dir/$1.pdf $out_dir/$1.dvi
+		scp -i ~/.ssh/id_rsa.netwalker output/note.pdf shirak@netwalker:Documents/share/.
+	fi
+fi
